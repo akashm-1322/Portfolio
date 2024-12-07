699419c7b25dc590fb550f6b701d0da8527876c5
akashm-1322 <67458231+akashm-1322@users.noreply.github.com>
2024-12-07 07:35:35 +0530

    Portfolio Updation part 2

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,9 +1,9 @@
 import React , {Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-const App = React.lazy( () => import('./App'));
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
+const App = React.lazy( () => import('./App'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
